004c6c6f4340a899d1689273d06eaa68828656a6
Xavier Xiqués <xavier.xiques@gmail.com>
2017-08-03 18:57:11 +0200

    Indent

--- index.ios.js
+++ index.ios.js
@@ -7,7 +7,7 @@ import Header from './src/components/header';
 
 // Create a component
 const App = () => (
-    <Header />
+	<Header />
 );
 
 // Render it to a device
--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,25 @@
 // Import libraries for making a component
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 // Make a component
 const Header = () => {
-    const { textStyle } = styles;
+	const { textStyle, viewStyle } = styles;
 
-    return <Text style={textStyle}>Albums!</Text>;
+	return (
+		<View style={viewStyle}>
+			<Text style={textStyle}>Albums!</Text>
+		</View>
+	);
 };
 
 const styles = {
-    textStyle: {
-        fontSize: 20
-    }
+	viewStyle: {
+		backgroundColor: '#F8F8F8'
+	},
+	textStyle: {
+		fontSize: 20
+	}
 };
 
 // Make the component available to other parts of the app
