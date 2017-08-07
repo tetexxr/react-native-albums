3922f8f1a3c5732dcc082117f47ff55ded95cc71
Xavier Xiqués <xavier.xiques@gmail.com>
2017-08-07 18:07:30 +0200

    Fetch and render albums

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class AlbunList extends Component {
+  state = { albums: [] };
+
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({ albums: response.data }));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album => <Text>{album.title}</Text>);
+  }
+
   render() {
     return (
       <View>
-        <Text>Album list!!</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
