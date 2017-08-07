7fb0bde6160d9c1de59d10ff424398833dd8118c
Xavier Xiqués <xavier.xiques@gmail.com>
2017-08-07 10:52:26 +0200

    Functional component to class based component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
-const AlbunList = () => {
-  return (
-    <View>
-      <Text>Album list!!</Text>
-    </View>
-  );
-};
+class AlbunList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Album list!!</Text>
+      </View>
+    );
+  }
+}
 
 export default AlbunList;
